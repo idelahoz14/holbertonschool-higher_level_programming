17422d92cb774b8bffcc478856bfcf5bdc2b8673
idelahoz14 <idelahzo14@cuc.edu.co>
2020-09-08 22:33:04 +0000

    0x03.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    count = 0
+    for i in my_list:
+        count += 1
+    if idx < count:
+        return my_list[idx]
+    else:
+        return None
