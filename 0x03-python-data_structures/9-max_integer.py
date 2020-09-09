0643b4c9a642d2258eef0fbf2b94469184ec7940
idelahoz14 <idelahzo14@cuc.edu.co>
2020-09-09 02:45:54 +0000

    0x03.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        maxi = my_list[0]
+        for i in my_list:
+            if i > maxi:
+                maxi = n
+        return maxi
+    else:
+        return None
