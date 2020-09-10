c00c60c9bc1163391ac8717a2b90d01ce4a3ae84
idelahoz14 <idelahzo14@cuc.edu.co>
2020-09-10 23:32:19 +0000

    Change variable name

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if i in a_dictionary:
-        a_dictionary.pop(i)
+    if key in a_dictionary:
+        a_dictionary.pop(key)
     return a_dictionary
