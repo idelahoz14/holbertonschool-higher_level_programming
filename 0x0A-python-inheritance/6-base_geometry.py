4c15ca65a6bc1db01fac085bd0d5ae763140298b
idelahoz14 <idelahzo14@cuc.edu.co>
2020-10-07 01:54:10 +0000

    Adding a new method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Represent BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """Represent BaseGeometry"""
+    def area(self):
+        """Represent area
+        Raises:
+            Exception: (area() is not implemented)
+        """
+        raise Exception("area() is not implemented")
