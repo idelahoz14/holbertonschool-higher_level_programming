37c673c42d47eee4b9531b704b97feb97656ca78
idelahoz14 <idelahoz14@cuc.edu.co>
2020-11-17 00:02:28 +0000

    task number 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- Script that prints the full description of the tables
-SELECT *
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
