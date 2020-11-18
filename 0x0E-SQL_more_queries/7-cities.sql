ecfd97ba99196a2aeaa68771e551c741cae28f53
idelahoz14 <idelahoz14@cuc.edu.co>
2020-11-18 01:50:32 +0000

    fixing code

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,9 @@
 -- Script that create a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, state_id NOT NULL FOREIGN KEY, name VARCHAR(256) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.cities(id)
 )
