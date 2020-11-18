063407b6e677e699f50601bcaddf02519f6865fc
idelahoz14 <idelahoz14@cuc.edu.co>
2020-11-18 02:26:19 +0000

    fixing code

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,5 @@
 -- Script that lists all cities contained in a database
-SELECT cities.id, cities.name, states.name, FROM cities, INNER JOIN states ON cities.state_id=states.id, ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id=states.id
+ORDER BY cities.id;
