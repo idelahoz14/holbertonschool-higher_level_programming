ceabe99ea5d78e95455862142f0728f960f55579
idelahoz14 <idelahoz14@cuc.edu.co>
2020-11-18 01:30:01 +0000

    fixing code

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Script that create a database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-    id INT NOT NULL UNIQUE, name VARCHAR(256)
+    id INT NOT NULL UNIQUE, name VARCHAR(256) NOT NULL
 )
