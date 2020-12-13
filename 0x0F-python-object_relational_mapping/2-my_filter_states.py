4658a0d1cc8b63b0945a01e7978aafd4944d6c3c
idelahoz14 <idelahoz14@cuc.edu.co>
2020-12-13 07:38:05 +0000

    switch comments

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Script that lists all states with a name starting with N (upper N) from the database """
+"""  Script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+ """
 
 import MySQLdb
 from sys import argv
