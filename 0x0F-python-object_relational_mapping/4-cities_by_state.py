483df5de03f005efc2a57aa6ab36412f66dcd99f
idelahoz14 <idelahoz14@cuc.edu.co>
2020-12-13 07:49:50 +0000

    fixing code(deleting statements

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,6 @@ if __name__ == '__main__':
     username = argv[1]
     password = argv[2]
     database = argv[3]
-    name = argv[4]
 
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
