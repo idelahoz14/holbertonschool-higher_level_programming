034c81c72f8b191bae51447ec41ad03555317658
idelahoz14 <idelahoz14@cuc.edu.co>
2020-12-13 07:26:30 +0000

    fixing code(adding equals)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        if row[1][0] = 'N':
+        if row[1][0] == 'N':
             print(row)
     cur.close()
     db.close()
