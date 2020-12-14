fa29107dbe2307f015fc88418c8984d97e909fb0
idelahoz14 <idelahoz14@cuc.edu.co>
2020-12-14 04:25:41 +0000

    model class

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Class Models """
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """ Constructor """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    
