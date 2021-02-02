18c2952a621fa4c763964e91508118cb048ac113
idelahoz14 <idelahoz14@cuc.edu.co>
2021-02-02 21:27:49 +0000

    task number 5(An Integer)

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Script that print a number
+
+const argument = parseInt(process.argv[2]);
+if (argument * 1 === argument) {
+  console.log('My number: ' + argument);
+} else {
+  console.log('Not number');
+}
