a00c8454e826cf0cf92b3d1efe4de0dcdab0ecd0
idelahoz14 <idelahoz14@cuc.edu.co>
2021-02-02 21:09:48 +0000

    task number 1(languajes)

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script that print 3 lines
+
+const c = 'C is fun';
+const python = 'Python is cool';
+const JS = 'Javascript is amazing';
+
+console.log(c, '\n' + python, '\n' + JS);
