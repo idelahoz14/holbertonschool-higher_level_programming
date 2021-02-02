1aa140fa3f487d5342b5e6e999a22915d0e1b820
idelahoz14 <idelahoz14@cuc.edu.co>
2021-02-02 22:42:33 +0000

    task number 9(Add)

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Script that prints the addition of 2 integers
+
+function add(a, b) {
+  const num1 = parseInt(a);
+  const num2 = parseInt(b);
+  if ((num1 * 1 !== num1) || (num2 * 1 !== num2)) {
+    return NaN;
+  } else {
+    return num1 + num2;
+  }
+}
+console.log(add(process.argv[2], process.argv[3]));
