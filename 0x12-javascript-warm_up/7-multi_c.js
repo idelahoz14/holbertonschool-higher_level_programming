21dde9e2e11b8df995dc1a3f123b5e6f69ab05e4
idelahoz14 <idelahoz14@cuc.edu.co>
2021-02-02 21:50:52 +0000

    fixing code(adding parseInt)

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Script that prints x times “C is fun”
 
-const x = process.argv[2];
+const x = parseInt(process.argv[2]);
 if (process.argv[1]) {
   console.log('Missing number of occurrences');
 }
