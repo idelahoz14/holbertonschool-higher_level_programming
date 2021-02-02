100b18b5b49d55ad7bacd199667097ef5b05e3bf
idelahoz14 <idelahoz14@cuc.edu.co>
2021-02-02 21:28:43 +0000

    task number 6( Loop to languages)

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Script that print an array of a startin loop
+
+const Array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < Array.length; i++) {
+  console.log(Array[i]);
+}
