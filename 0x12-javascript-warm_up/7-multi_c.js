c9ceb15fdf823167151c920421de482ccee7026e
idelahoz14 <idelahoz14@cuc.edu.co>
2021-02-02 21:47:28 +0000

    task number 7(I love C)

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that prints x times “C is fun”
+
+const x = process.argv[2];
+if (process.argv[1]) {
+  console.log('Missing number of occurrences');
+}
+for (let i = 0; i < x; i++) {
+  console.log('C is fun');
+}
