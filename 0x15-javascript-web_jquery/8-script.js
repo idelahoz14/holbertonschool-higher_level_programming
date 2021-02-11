2875611d5444cd95cef1ebf92e343cc3b4e20441
idelahoz14 <idelahoz14@cuc.edu.co>
2021-02-11 01:59:43 +0000

    tasks of project JS - Web Jquery

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(function(){
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function(data) {
+    for (let i = 0; i < data.results.length; i++) {
+      $('<li>' + data.results[i].title + '</li>').appendTo('#list_movies');
+    }
+  });
+});
