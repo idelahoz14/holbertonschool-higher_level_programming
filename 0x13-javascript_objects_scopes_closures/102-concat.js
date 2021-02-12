0c009b28be7ea4b930e693cbe452595ef1d1fb18
idelahoz14 <idelahoz14@cuc.edu.co>
2021-02-12 15:08:08 +0000

    advanced task

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that concats 2 files
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+const fs = require('fs');
+const textA = fs.readFileSync(fileA, 'utf8');
+const textB = fs.readFileSync(fileB, 'utf8');
+fs.writeFileSync(fileC, textA + textB);
