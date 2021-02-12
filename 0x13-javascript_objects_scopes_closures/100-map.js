468d3fd598b49533453fe67320e217c24a4a1417
idelahoz14 <idelahoz14@cuc.edu.co>
2021-02-12 14:44:42 +0000

    fixing code(semistandard style)

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 // Script that imports an array and computes a new array
 
 const list = require('./100-data.js').list;
-const list_return = list.map((x, i) => x * i);
+const x = list.map((x, i) => x * i);
 
 console.log(list);
-console.log(list_return);
+console.log(x);
